5a660ebb9b8bb798666c3160e488b68ef2c2383b
disoul <starky_ds@sina.cn>
2015-04-20 05:33:25 +0800

    ProvinceMapActivity: 基本实现

--- app/src/main/java/musetest/disoul/com/musetest/ProvinceMapActivity.java
+++ app/src/main/java/musetest/disoul/com/musetest/ProvinceMapActivity.java
@@ -2,15 +2,25 @@ package musetest.disoul.com.musetest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class ProvinceMapActivity extends ActionBarActivity {
 
     private static final String ARG_PROVINCE_NAME = "musetest.com.disoul.musetest.PROVINCE_NAME";
+
+    @InjectView(R.id.provinceImage)
+    ImageView provinceImage;
+
+    private String provinceName;
 
     public static void launch(final Context ctx, final String provinceName) {
         Intent intent = new Intent(ctx, ProvinceMapActivity.class);
@@ -22,6 +32,18 @@ public class ProvinceMapActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_province_map);
+        ButterKnife.inject(this);
+
+        final Intent intent = getIntent();
+        provinceName = intent.getStringExtra(ARG_PROVINCE_NAME);
+
+        provinceImage.setImageDrawable(getProvinceImageDrawable(provinceName));
+    }
+
+    @SuppressWarnings("deprecation")
+    private Drawable getProvinceImageDrawable(final String provinceName) {
+        final int resId = getResources().getIdentifier("province_img_" + provinceName, "drawable", getPackageName());
+        return getResources().getDrawable(resId);
     }
 
     @Override
